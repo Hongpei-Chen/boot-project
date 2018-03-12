f7affaf099a2ffc96cadf6bc16e54f26be7b4703
jeff <jeffchen@techno-vision.cn>
2018-03-12 18:38:27 +0800

    xxxx

---
+++ src/main/java/com/jeff/bootproject/bean/Pepole.java
@@ -0,0 +1,16 @@
+package com.jeff.bootproject.bean;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author jeff
+ * <p>Date 2018/3/12 14:17</p>
+ */
+@Data
+@ToString
+public class Pepole {
+
+    private String name;
+    private Integer age;
+}
